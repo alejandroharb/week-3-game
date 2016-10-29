f53a29aba4126aeef19ba830e7751815331d4c72
alejandroharb <alejandroharb@gmail.com>
2016-10-29 17:56:42 -0500

    started html and pseudo code of js

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// 1. Generate random word
+// 2. display blanks for number of letters in word
+// 3. detect letters chosen by user
+//      a. if letter matches any of the letters in the word,
+//          then display letter of the word.
+//      b. else, add letter to a bank of letters chosen
+//          i. if word has already been chosen before, do not allow it to be chosen again.
+// 4.
