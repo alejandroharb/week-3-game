10edc28c60922e7536de0d37dce7a325f7eae438
alejandroharb <alejandroharb@gmail.com>
2016-11-01 16:19:34 -0500

    added conditions for array loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,51 +12,57 @@
 
 var wordBank = ["Argentina","Bolivia","Brazil","Chile","Colombia","Venezuela","Ecuador","Guyana","Peru","Paraguay","Suriname", "Uruguay","Venezuela", "Canada", "Cuba", "Guatemala", "Jamaica", "Mexico", "Panama", "Nicaragua", "Austria", "Belgium", "Bosnia", "Bulgaria", "Croatia", "Denmark", "France", "Georgia", "Germany", "Greece", "Hungary", "Italy", "Iceland", "Kazakhstan", "Netherlands", "Norway", "Poland", "Portugal", "Russia", "Spain", "Sweden", "Turkey", "Afghanistan", "Cambodia", "China", "India", "Japan", "Israel", "Iran", "Kazakhstan", "Lebanon", "Malaysia", "Pakistan", "Palestine", "Syria", "Thailand", "Vietnam" ,"Algeria", "Chad", "Egypt", "Ethiopia", "Kenya", "Liberia", "Lybia", "Madagascar", "Rwanda", "Nigeria", "Uganda", "Sudan", "Zimbabwe"]
 
-// Random word is chosen from word bank
-// words turned into lower case
-var randomWord = wordBank[Math.floor(Math.random()*wordBank.length)].toLowerCase();
-
-// random word is made into an array of each letter and space
-var randomWordArr = randomWord.split("");
-console.log(randomWord);
-console.log(randomWordArr);
-console.log("-------------------");
-
-//array to store the blank letters of the random word
-var blankWordArr = [];
-
-// loop adds "_ " for each letter of randomWord
-for (var i = 0; i<randomWordArr.length; i++) {
-	blankWordArr.push("_ ");
-}
-console.log(blankWordArr);
-console.log("-------------------");
-
-//To display blank word, join array into a string
-var blankWordDisplay = blankWordArr.join(' ');
-console.log(blankWordDisplay);
-
-console.log("-------------------");
-var userGuessArr = []
-document.addEventListener('keyup', function(event){
+var gameStart = document.addEventListener('keyup', function(event){
 	// to detect which key was pressed and store it in userGuess
 	if ((event.keyCode == 13) || (event.keyCode == 32)){ //space or enter start game
 		alert("game begins");
+
+
+		document.removeEventListener('keyupd', gameStart);
+		// Random word is chosen from word bank
+		// words turned into lower case
+		var randomWord = wordBank[Math.floor(Math.random()*wordBank.length)].toLowerCase();
+
+		// random word is made into an array of each letter and space
+		var randomWordArr = randomWord.split("");
+		console.log(randomWord);
+		console.log(randomWordArr);
+		console.log("-------------------");
+
+		//array to store the blank letters of the random word
+		var blankWordArr = [];
+
+		// loop adds "_ " for each letter of randomWord
+		for (var i = 0; i<randomWordArr.length; i++) {
+			blankWordArr.push("_ ");
+		}
+		console.log(blankWordArr);
+		console.log("-------------------");
+
+		//To display blank word, join array into a string
+		var blankWordDisplay = blankWordArr.join(' ');
+		console.log(blankWordDisplay);
+
+		console.log("-------------------");
+		var userGuessArr = []
+		var wrongUserGuessArr = [];
 		document.addEventListener('keyup', function(event){
 			var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-			
-			if ((event.keyCode <=90) && (event.keyCode >= 65)) { // to detect only numbers
-				for(var i = 0; i<randomWordArr.length;i++){
-					if (userGuess === randomWordArr[i]) {
-						console.log("letter correct!")
-						userGuessArr.push(userGuess);
-					} else {
-						console.log("letter not in word.")
+			if(userGuessArr.sort() !== randomWordArr.sort()) {
+				if ((event.keyCode <=90) && (event.keyCode >= 65)) { // to detect only numbers
+					for(var i = 0; i<randomWordArr.length;i++){
+						if (userGuess == randomWordArr[i]) {
+							console.log("Correct!");
+							userGuessArr.push(userGuess);
+						}
 					}
+				console.log("rightly guessed: " + userGuessArr);
+				console.log("Wrongly guessed: " + wrongUserGuessArr);
 				}
-			console.log(userGuessArr);
+			} else {
+				alert("You've won!");
 			}
-		})
+		});
 	}
 });
 
