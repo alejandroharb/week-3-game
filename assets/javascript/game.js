a523d8c68123ef967ffbc4a1d18f3d582c746461
alejandro harb <alejandro harb>
2016-11-02 10:00:21 -0500

    added functions to clean up code, added to pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,37 @@
-// 1. Have word bank with them
-// 2 Generate random word from word bank
-// 2. display blanks for number of letters in word
-// 		a. determine x number of characters in word
-// 		b. push  x "_" into an array
+// 1. Word Bank
+// 2  Generate random word from word bank: 
+// 3. Make random word into array of letters
+// 3. display blanks for number of letters in word
+// 		a. determine x number of characters in random word
+// 		b. push  "_" into a new array: blankWord
+// 4. make variable for blank word display (blankWordDisplay): join blanks together 
 // 3. detect letters chosen by user
 //      a. if letter matches any of the letters in the word,
 //          then display letter of the word.
+// 			i. add into array for correctUserGuess
 //      b. else, add letter to a bank of letters chosen
 //          i. if word has already been chosen before, do not allow it to be chosen again.
-// 4.
+// toDo:
+// prevent word storing in userGuessArr after it's been chosen once
+// show bank of letters chosen already
+// begin displaying game in DOM
 
 var wordBank = ["Argentina","Bolivia","Brazil","Chile","Colombia","Venezuela","Ecuador","Guyana","Peru","Paraguay","Suriname", "Uruguay","Venezuela", "Canada", "Cuba", "Guatemala", "Jamaica", "Mexico", "Panama", "Nicaragua", "Austria", "Belgium", "Bosnia", "Bulgaria", "Croatia", "Denmark", "France", "Georgia", "Germany", "Greece", "Hungary", "Italy", "Iceland", "Kazakhstan", "Netherlands", "Norway", "Poland", "Portugal", "Russia", "Spain", "Sweden", "Turkey", "Afghanistan", "Cambodia", "China", "India", "Japan", "Israel", "Iran", "Kazakhstan", "Lebanon", "Malaysia", "Pakistan", "Palestine", "Syria", "Thailand", "Vietnam" ,"Algeria", "Chad", "Egypt", "Ethiopia", "Kenya", "Liberia", "Lybia", "Madagascar", "Rwanda", "Nigeria", "Uganda", "Sudan", "Zimbabwe"]
 
 var gameStart = document.addEventListener('keyup', function(event){
 	// to detect which key was pressed and store it in userGuess
-	if ((event.keyCode == 13) || (event.keyCode == 32)){ //space or enter start game
-		alert("game begins");
-
+	if (event.keyCode == 32){ //space or enter start game
+		alert("game begins!");
 
 		document.removeEventListener('keyupd', gameStart);
-		// Random word is chosen from word bank
-		// words turned into lower case
+	
+		// Random word is chosen from word bank. letters turned into lower case
 		var randomWord = wordBank[Math.floor(Math.random()*wordBank.length)].toLowerCase();
 
-		// random word is made into an array of each letter and space
+		// random word is made into an array of each letter
 		var randomWordArr = randomWord.split("");
-		console.log(randomWord);
-		console.log(randomWordArr);
+		console.log("Random Word is: " + randomWord);
+		console.log("Random Word in Array: " + randomWordArr);
 		console.log("-------------------");
 
 		//array to store the blank letters of the random word
@@ -41,28 +46,36 @@ var gameStart = document.addEventListener('keyup', function(event){
 
 		//To display blank word, join array into a string
 		var blankWordDisplay = blankWordArr.join(' ');
-		console.log(blankWordDisplay);
+		console.log("Word Blank: " + blankWordDisplay);
 
 		console.log("-------------------");
 		var userGuessArr = []
 		var wrongUserGuessArr = [];
 		document.addEventListener('keyup', function(event){
-			var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-			if(userGuessArr.sort() !== randomWordArr.sort()) {
-				if ((event.keyCode <=90) && (event.keyCode >= 65)) { // to detect only numbers
-					for(var i = 0; i<randomWordArr.length;i++){
-						if (userGuess == randomWordArr[i]) {
-							console.log("Correct!");
-							userGuessArr.push(userGuess);
+			if ((event.keyCode <=90) && (event.keyCode >= 65)) { // to detect only numbers
+				var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+				console.log("-------FUNCTION TEST------------");
+				function checkLetter (checkArr,letter,correctBankArr) {
+					for(var i = 0; i <checkArr.length; i++) {
+						if(letter == checkArr[i]) {
+							console.log("letter " + letter + " found")
+							correctBankArr.push(letter);
 						}
 					}
-				console.log("rightly guessed: " + userGuessArr);
-				console.log("Wrongly guessed: " + wrongUserGuessArr);
+				};
+				checkLetter(randomWordArr, userGuess, userGuessArr);
+				console.log(userGuessArr);
+
+				console.log(userGuessArr.sort());
+				console.log(randomWordArr.sort());
+
+				var compareArrays = function (randomArr,userArr){
+					if((userArr.sort().toString()) == (randomArr.sort().toString())){
+						alert("You've won!");
+					}
 				}
-			} else {
-				alert("You've won!");
+				compareArrays(randomWordArr,userGuessArr);
 			}
 		});
 	}
 });
-
