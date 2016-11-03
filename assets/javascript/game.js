093452cac78ef0a6d5727dea7fd96daf67c84b04
alejandroharb <alejandroharb@gmail.com>
2016-11-03 16:56:25 -0500

    finishing code for raw game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 // 1. Word Bank
-// 2  Generate random word from word bank: 
+// 2  Generate random word from word bank:
 // 3. Make random word into array of letters
 // 3. display blanks for number of letters in word
 // 		a. determine x number of characters in random word
 // 		b. push  "_" into a new array: blankWord
-// 4. make variable for blank word display (blankWordDisplay): join blanks together 
+// 4. make variable for blank word display (blankWordDisplay): join blanks together
 // 3. detect letters chosen by user
 //      a. if letter matches any of the letters in the word,
 //          then display letter of the word.
@@ -12,8 +12,8 @@
 //      b. else, add letter to a bank of letters chosen
 //          i. if word has already been chosen before, do not allow it to be chosen again.
 // toDo:
-// prevent word storing in correctGuessArr after it's been chosen once
-// show bank of letters chosen already
+// remove "game start" event listener
+// Add countdown of available Guesses (The hangman)
 // begin displaying game in DOM
 
 var wordBank = ["Argentina","Bolivia","Brazil","Chile","Colombia","Venezuela","Ecuador","Guyana","Peru","Paraguay","Suriname", "Uruguay","Venezuela", "Canada", "Cuba", "Guatemala", "Jamaica", "Mexico", "Panama", "Nicaragua", "Austria", "Belgium", "Bosnia", "Bulgaria", "Croatia", "Denmark", "France", "Georgia", "Germany", "Greece", "Hungary", "Italy", "Iceland", "Kazakhstan", "Netherlands", "Norway", "Poland", "Portugal", "Russia", "Spain", "Sweden", "Turkey", "Afghanistan", "Cambodia", "China", "India", "Japan", "Israel", "Iran", "Kazakhstan", "Lebanon", "Malaysia", "Pakistan", "Palestine", "Syria", "Thailand", "Vietnam" ,"Algeria", "Chad", "Egypt", "Ethiopia", "Kenya", "Liberia", "Lybia", "Madagascar", "Rwanda", "Nigeria", "Uganda", "Sudan", "Zimbabwe"]
@@ -29,13 +29,16 @@ var randomWordArr = randomWord.split("");
 var noRepeat = true;
 var usedLettersArr = [];
 var correctGuessArr = [];
-
-var gameStart = document.addEventListener('keyup', function(event){
-	// to detect which key was pressed and store it in userGuess
-	if (event.keyCode == 32){ //space start game 
+var guessCount = 7;
+var gameLose = false;
+var gameWin = false;
+document.addEventListener('keyup', startGame);
+function startGame(event){
+		// to detect which key was pressed and store it in userGuess
+	if (event.keyCode == 32){ //space start game
 		alert("game begins!");
 
-		// document.removeEventListener('keyup', gameStart);
+		document.removeEventListener('keyup', startGame);
 		console.log("Random Word is: " + randomWord);
 		console.log("Random Word in Array: " + randomWordArr);
 		console.log("-------------------");
@@ -54,27 +57,33 @@ var gameStart = document.addEventListener('keyup', function(event){
 		document.addEventListener('keyup', function(event){
 			if ((event.keyCode <=90) && (event.keyCode >= 65)) { // to detect only numbers
 				var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+				if(guessCount > 0){
+					guessCount--;
+				}
+				if (guessCount == 0) {
+					gameLose = true;
+				}
 				usedLettersArr.push(userGuess);
-
-				console.log("-------Repeat TEST------------");
-				var letterRepeat = function (usedArr, letter){
-					for(var i = 0; i < usedArr.length; i++){
-						if (letter == usedArr[i]){
+				console.log(guessCount);
+				console.log("-------Letter REPEAT TEST------------");
+				var letterRepeat = function (usedLetterArr, letterChoice){
+					for(var i = 1; i < usedLetterArr.length - 1; i++){
+						if (letterChoice === usedLetterArr[i]){
 							noRepeat = false;
 						} else {
 							noRepeat = true;
 						}
-					} 
+					} // **This for loop looks at only letters guessed in the past, by starting at array index 1 (so the first guess is not wrongly detected), and doesn't look at the last index, since every guess gets pushed to the back of the array.
 
 				};
 				letterRepeat(usedLettersArr, userGuess);
 				console.log("Repeat function Bolean: " + noRepeat)
 				console.log("-------FUNCTION TEST------------");
-				function checkLetter (checkArr,letter,correctBankArr) {
-					for(var i = 0; i <checkArr.length; i++) {
-						if((letter == checkArr[i])&& (noRepeat)) {
-							console.log("letter " + letter + " found")
-							correctBankArr.push(letter);
+				function checkLetter (computerArr,letterChoice,correctBankArr) {
+					for(var i = 0; i <computerArr.length; i++) {
+						if((letterChoice == computerArr[i]) && (noRepeat)) {
+							console.log("letter " + letterChoice + " found")
+							correctBankArr.push(letterChoice);
 						}
 					}
 				};
@@ -85,13 +94,17 @@ var gameStart = document.addEventListener('keyup', function(event){
 				console.log("Correct Guesses Array Sorted" + correctGuessArr.sort());
 				console.log("Computer Word Array Sorted" + randomWordArr.sort());
 
-				var compareArrays = function (randomArr,userArr){
-					if((userArr.sort().toString()) == (randomArr.sort().toString())){
+				//Detect when user Wins
+				var compareArrays = function (computerArr,userArr){
+					if((userArr.sort().toString()) == (computerArr.sort().toString())){
 						alert("You've won!");
+						gameWin = true;
 					}
 				}
 				compareArrays(randomWordArr,correctGuessArr);
 			}
 		});
 	}
-});
+}
+
+
